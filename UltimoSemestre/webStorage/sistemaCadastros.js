8a4f9ac263df2af99617abbfd64222c429be9b83
GoibaGoiba <enzzocossolino@gmail.com>
2026-08-20 13:46:13 -0300

    adiciona funcionalidade de persistência de dados no localStorage e corrige exibição de produtos

--- UltimoSemestre/webStorage/sistemaCadastros.js
+++ UltimoSemestre/webStorage/sistemaCadastros.js
@@ -1,27 +1,30 @@
 class Estoque {
   constructor() {
     this.produtos = [];
+    this.carregarLocalStorage();
   }
 
   adicionarProduto(produto) {
     this.produtos.push(produto);
+    localStorage.setItem("meuEstoque", JSON.stringify(this.produtos));
   }
 
   aplicarDesconto(produto) {
     const precoDesconto =
       produto.preco - (produto.preco * produto.desconto) / 100;
-
     return precoDesconto;
   }
+
   excluirProduto(indice) {
     this.produtos.splice(indice, 1);
+    localStorage.setItem("meuEstoque", JSON.stringify(this.produtos));
     this.exibirNaTela();
   }
 
-  exibirNaTela(produto) {
+  exibirNaTela() {
     const resultadoProduto = document.querySelector("#resultadoProduto");
-
     resultadoProduto.innerHTML = "";
+    
     this.produtos.forEach((produto, indice) => {
       resultadoProduto.innerHTML += `
         <div>
@@ -33,8 +36,8 @@ class Estoque {
             <button class="excluirProduto" data-indice="${indice}">[Excluir]</button>
         </div>`;
     });
-    const botoes = resultadoProduto.querySelectorAll(".excluirProduto");
 
+    const botoes = resultadoProduto.querySelectorAll(".excluirProduto");
     botoes.forEach((botao) => {
       botao.addEventListener("click", () => {
         const indice = Number(botao.dataset.indice);
@@ -42,7 +45,14 @@ class Estoque {
       });
     });
   }
-}
+
+  carregarLocalStorage() {
+    const dadosSalvos = localStorage.getItem("meuEstoque");
+    if (dadosSalvos) {
+      this.produtos = JSON.parse(dadosSalvos);
+    }
+  }
+} 
 
 class Produto {
   constructor(nome, preco, categoria, desconto) {
@@ -54,12 +64,14 @@ class Produto {
 }
 
 const estoque = new Estoque();
+
+estoque.exibirNaTela();
+
 const nomeProduto = document.querySelector("#nomeProduto");
 const precoProduto = document.querySelector("#precoProduto");
 const categoriaProduto = document.querySelector("#categoriaProduto");
 const descontoProduto = document.querySelector("#descontoProduto");
 const botaoCadastrarProduto = document.querySelector("#botaoCadastrarProduto");
-// const resultadoProduto = document.querySelector("#resultadoProduto");
 
 botaoCadastrarProduto.addEventListener("click", function () {
   const produto = new Produto(
@@ -70,5 +82,5 @@ botaoCadastrarProduto.addEventListener("click", function () {
   );
 
   estoque.adicionarProduto(produto);
-  estoque.exibirNaTela(produto);
+  estoque.exibirNaTela();
 });
